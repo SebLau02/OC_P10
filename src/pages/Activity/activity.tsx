793464eac46cea4cee932d06a7b018300925ee5e
Sébastien Lau <sebastien.lau2021@gmail.com>
2026-07-13 21:54:11 +0200

    refactor: simplifier le composant Activity en supprimant les filtres et le sélecteur

--- src/pages/Activity/activity.tsx
+++ src/pages/Activity/activity.tsx
@@ -1,43 +1,6 @@
-import { useState } from "react";
-import { DailyActivities, Select } from "../../components";
-
-const DISPLAY_OPTIONS = [
-  {
-    label: "Jour",
-    value: 1,
-  },
-  {
-    label: "Semaine",
-    value: 2,
-  },
-  {
-    label: "Mois",
-    value: 3,
-  },
-  {
-    label: "Année",
-    value: 4,
-  },
-];
-
-type FiltersType = {
-  display: number | string;
-};
-
-const defaultFilters: FiltersType = {
-  display: 1,
-};
+import { DailyActivities } from "../../components";
 
 function Activity() {
-  const [filters, setFilters] = useState<FiltersType>(defaultFilters);
-
-  const handleChange = (e: {
-    target: { name: string; value: string | number };
-  }) => {
-    const { value, name } = e.target;
-    setFilters((prev) => ({ ...prev, [name]: value }));
-  };
-
   return (
     <main
       className="py-8 mx-auto"
@@ -46,15 +9,6 @@ function Activity() {
         width: "100%",
       }}
     >
-      <div className="d-flex flex-row flex-wrap mb-4">
-        <Select
-          label="Par"
-          options={DISPLAY_OPTIONS}
-          value={filters.display}
-          onChange={handleChange}
-          name="display"
-        />
-      </div>
       <DailyActivities />
     </main>
   );
